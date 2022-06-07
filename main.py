84491f289616f504be6731f52d06d4b69b59577f
peatoe <cowlespeter86@gmail.com>
2022-06-06 17:38:17 -0700

    Update main.py
    First working version
    --v0.1

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
+#make a program that asks the user for a list of things to do, then asks how many hours they want to work for the day and divide the time evenly amongst the tasks.
+#then it should print out the list of things to do, then the number of hours they should work on each thing.
 import time
 import datetime
 import os
 
 
 to_do_list = input("What is everything you would like to work on today? ")
+how_long = float(input("How many hours would you like to work on this? "))
 
+
+#convert each item in to_do_list into a list
 def make_list(to_do):
     li = list(to_do.split(", "))
     return li
    
 todays_list = (make_list(to_do_list))
 str1 = ', '.join(todays_list)
-print("Here is what you are working on today: " + str1)
 
-h1 = input("How long do you want to work on " + todays_list[0] + "?")
-h2 = input("How long do you want to work on " + todays_list[1] + "?")
+#evenly divide the number of items in the list to_do_list by the integer of how_long
+def divide_time(tasks, hours):
+    time_per_task = hours / len(tasks)
+    return time_per_task
+
+time_divide = divide_time(todays_list, how_long)
 
 os.system('cls' if os.name == 'nt' else 'clear')
+print("Today's list: " + str1)
+print("You should work on each task for " + str(time_divide) + " hours.")
 
-print("You are working on: " + todays_list[0])
+time.sleep(2)
 
+input("Press enter to start your first task: ")
+
+#timer for the task at hand
 def countdown(h):
-    total_seconds = h * 60
+    total_seconds = h * 3600
     while total_seconds > 0:
         timer = datetime.timedelta(seconds = total_seconds)
         print(timer, end="\r")
         time.sleep(1)
         total_seconds -= 1
-    print("Bzzzt! Time to move on to: " + todays_list[1])
+    print("Bzzzt! Time to move on to the next task!")
+    input("Press enter to continue: ")
 
-countdown(int(h1))
+#create a loop through the list of things to do
+for i in todays_list:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("You are working on: " + i)
+    countdown(time_divide)
+os.system('cls' if os.name == 'nt' else 'clear')
+print("You have completed your day!")
